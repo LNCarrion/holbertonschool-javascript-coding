c349b3165fb6f673464a8679b61dbc0a04ed0225
LNCarrion <lestycarrion@gmail.com>
2024-05-17 17:01:53 -0400

    fix 5 #2

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,4 +1,20 @@
 #!/usr/bin/node
 const fs = require('fs');
-const request = require('request');
-request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
+const axios = require('axios');
+
+const getAndStoreWebpage = async (url, filePath) => {
+    try {
+        const response = await axios.get(url);
+        fs.writeFileSync(filePath, response.data, { encoding: 'utf-8' });
+    } catch (error) {
+        console.error("Error:", error.message);
+    }
+};
+
+if (process.argv.length !== 4) {
+    console.log("Usage: node script.js <url> <file_path>");
+} else {
+    const url = process.argv[2];
+    const filePath = process.argv[3];
+    getAndStoreWebpage(url, filePath);
+}
