085920bf94e2ac8da555b897a3765e0beefeb61d
LNCarrion <lestycarrion@gmail.com>
2024-05-28 12:01:32 -0400

    fix

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,13 @@
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.setEncoding('utf-8');
 
 process.stdin.on('data', (data) => {
-
   const name = data.trim();
   process.stdout.write(`Your name is: ${name}\r\n`);
   process.stdin.end();
 });
 
 process.stdin.on('end', () => {
-  console.log("This important software is now closing");
+  console.log('This important software is now closing');
 });
