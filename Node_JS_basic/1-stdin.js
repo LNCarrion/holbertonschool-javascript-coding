e6dd098e358b8ee355b7c2add9df45e741841561
LNCarrion <lestycarrion@gmail.com>
2024-05-28 11:54:27 -0400

    fix file 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,7 +5,7 @@ process.stdin.setEncoding('utf-8');
 process.stdin.on('data', (data) => {
 
   const name = data.trim();
-  console.log(`Your name is: ${name}`);
+  console.log(`Your name is: ${name}\r`);
   process.stdin.end();
 });
 
